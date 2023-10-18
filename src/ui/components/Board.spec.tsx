48545dfcf39be35fc7027c688e1cd2e179ba8c48
Dean Radcliffe <24406+deanrad@users.noreply.github.com>
2023-10-18 15:37:00 -0500

    Working Board test

---
+++ src/ui/components/Board.spec.tsx
@@ -0,0 +1,48 @@
+import { describe, it, expect, vi } from "vitest";
+import { act, render } from "@testing-library/react";
+import { NOTATION_TOGGLE } from "@src/events/controls";
+import { Board } from "./Board";
+import { defaultBus } from "@rxfx/service";
+
+const mockBoard = vi.fn();
+
+vi.mock("react-chessboard", () => ({
+  Chessboard: (props) => {
+    mockBoard(props);
+    return <div>Showing notation: {props.showBoardNotation}</div>;
+  },
+}));
+
+describe(Board, () => {
+  describe("Upon a NOTATION_TOGGLE event", () => {
+    it("hides the notation", async () => {
+      render(<Board />);
+
+      // Test the right props were given to show, initially
+      expect(mockBoard).toHaveBeenCalledWith(
+        expect.objectContaining({
+          showBoardNotation: true,
+        })
+      );
+
+      await act(() => {
+        defaultBus.trigger(NOTATION_TOGGLE(true));
+      });
+
+      // Now assert, it's told not to show
+      expect(mockBoard).toHaveBeenNthCalledWith(
+        2,
+        expect.objectContaining({
+          showBoardNotation: false,
+        })
+      );
+
+      // OR
+      // Assert on the rendered output
+      // const firstSquare = result.container.querySelector(`[data-boardid=chessboard] > div > div`);
+      // console.log(firstSquare?.outerHTML);
+
+      // expect(firstSquare.innerText).toEqual("");
+    });
+  });
+});
